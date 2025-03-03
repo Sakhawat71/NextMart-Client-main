a7272fa7a2c7a800f00fe12e74821b876b7ad32b
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-03-04 02:13:58 +0600

    feat: add Avatar component and integrate it into Navbar with dropdown menu

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -2,6 +2,17 @@ import Logo from "@/app/assets/svgs/Logo";
 import { Button } from "../ui/button";
 import { Heart, ShoppingBag } from "lucide-react";
 import Link from "next/link";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+
 
 export default function Navbar() {
     return (
@@ -37,6 +48,23 @@ export default function Navbar() {
                             className="rounded-full"
                         >create store</Button>
                     </Link>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger>
+                            <Avatar>
+                                <AvatarImage src="https://github.com/shadcn.png" />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>Profile</DropdownMenuItem>
+                            <DropdownMenuItem>Billing</DropdownMenuItem>
+                            <DropdownMenuItem>Team</DropdownMenuItem>
+                            <DropdownMenuItem>Subscription</DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
                 </nav>
             </div>
         </header>
