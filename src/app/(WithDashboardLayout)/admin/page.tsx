d394c8e43dee91b08a4e39b538f3fb79bd34d778
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-03-05 02:24:34 +0600

    add Admin and User home pages with welcome messages

---
+++ src/app/(WithDashboardLayout)/admin/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminHomePage = () => {
+    return (
+        <div>
+            <h2>Welcome Admin</h2>
+        </div>
+    );
+};
+
+export default AdminHomePage;
