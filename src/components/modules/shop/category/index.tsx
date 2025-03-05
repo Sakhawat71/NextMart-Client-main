e886722ca99c91acce5fc07a799520284c61d158
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-03-06 01:58:11 +0600

    add ProductCategoryPage and ManageCategories component; remove UserHomePage

---
+++ src/components/modules/shop/category/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const ManageCategories = () => {
+    return (
+        <div>
+            <h2>Manage Categories</h2>
+            <Button>Create Categroy</Button>
+        </div>
+    );
+};
+
+export default ManageCategories;
