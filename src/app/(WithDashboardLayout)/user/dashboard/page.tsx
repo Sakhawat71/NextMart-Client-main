b7d26dc4a7a5bc10ba2c89ebc5d16bc699fb0d87
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-03-06 01:35:10 +0600

    streamline DashboardLayout and enhance UserDashboardPage structure

--- src/app/(WithDashboardLayout)/user/dashboard/page.tsx
+++ src/app/(WithDashboardLayout)/user/dashboard/page.tsx
@@ -3,9 +3,16 @@ import React from 'react';
 const UserDashboardPage = () => {
     return (
         <div>
-            
+            <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                    <div className="aspect-video rounded-xl bg-green-200" />
+                    <div className="aspect-video rounded-xl bg-green-200" />
+                    <div className="aspect-video rounded-xl bg-green-200" />
+                </div>
+                <div className="min-h-[100vh] flex-1 rounded-xl bg-slate-300 md:min-h-min" ></div>
+            </div>
         </div>
-    ); 
+    );
 };
 
 export default UserDashboardPage;
