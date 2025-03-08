83624c3c0fedabc178f769fe7a3c569438c397ba
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-03-08 18:36:12 +0600

    add FeaturedProducts component to home page; implement product fetching and display

--- src/app/(WithCommonLayout)/(home)/page.tsx
+++ src/app/(WithCommonLayout)/(home)/page.tsx
@@ -1,4 +1,5 @@
 import Category from "@/components/modules/home/Category";
+import FeaturedProducts from "@/components/modules/home/FeaturedProducts";
 import HeroSection from "@/components/modules/home/HeroSection";
 
 const HomePage =async () => {
@@ -7,6 +8,7 @@ const HomePage =async () => {
         <div>
             <HeroSection />
             <Category />
+            <FeaturedProducts />
         </div>
     );
 };
