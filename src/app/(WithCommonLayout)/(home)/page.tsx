0005dab5d8d432fbd30f212982c48ffea8859cb2
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-03-09 20:44:23 +0600

    add FlashSale component with countdown timer and product display; integrate into home page

--- src/app/(WithCommonLayout)/(home)/page.tsx
+++ src/app/(WithCommonLayout)/(home)/page.tsx
@@ -1,14 +1,16 @@
 import Category from "@/components/modules/home/Category";
 import FeaturedProducts from "@/components/modules/home/FeaturedProducts";
+import FlashSale from "@/components/modules/home/FlashSale";
 import HeroSection from "@/components/modules/home/HeroSection";
 
-const HomePage =async () => {
+const HomePage = async () => {
 
     return (
         <div>
             <HeroSection />
             <Category />
             <FeaturedProducts />
+            <FlashSale />
         </div>
     );
 };
